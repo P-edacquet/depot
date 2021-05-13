51f873ae0887fc46d82de93538f34fac57dd3531
Pierre-emmanuel Dacquet <pierre-emmanueldacquet@MacBook-Air-de-Pierre-emmanuel.local>
2021-05-13 19:46:31 +0200

    Validation and Unit Testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+    validates :title, uniqueness: true
+    validates :title, :description, :image_url, presence: true
+    validates :image_url, allow_blank: true, format: {
+        with:    %r{\.(gif|jpg|png)\z}i,
+        message:    'must be a URL for GID, JPG or PNG image.'
+    }
+    validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
